2b019a65f5ff1c2caf7a04f5b4f789a55b723946
Rowlkh <rkhaledx@gmail.com>
2024-08-16 23:00:00 +0300

    header 100%

--- src/components/header.js
+++ src/components/header.js
@@ -5,16 +5,23 @@ import './header.css';
 
 function Header() {
     return (
-        <header className="header"> {}
-            <h1>ROAA KHALED</h1>
-            <i class="fa-regular fa-envelope">rkhaledx@gmail.com</i>
-            <div className="Bar">
+        <header className="header"> { }
+            <div className='row-1'>
+                <h1>ROAA KHALED</h1>
+                <div className="email">
+                    <a href="mailto:rkhaledx@gmail.com">
+                        <i className="fas fa-envelope"></i>
+                        <span >rkhaledx@gmail.com</span>
+                    </a>
+                </div>
+
+            </div>
+            <div className='row-2'>
                 <h3>My Projects</h3>
                 <h3>Collaborative Projects </h3>
                 <h3>Work Experience</h3>
                 <h3>Courses Taken</h3>
                 <h3>Skills</h3>
-
             </div>
         </header>
     );
